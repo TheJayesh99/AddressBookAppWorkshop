3d04e52a469b0d17d1fc14da6c9fd8d4e5f2db30
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-17 19:21:12 +0530

    Added : Method to store contact in local storage

--- js/ConatctForm.js
+++ js/ConatctForm.js
@@ -49,8 +49,28 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function save() {
-    let contact = new Contact()
-    contact.id = new Date().getTime()
+  try {
+    let contact = createContact();
+    craeteAndUpdateStorage(contact)
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function craeteAndUpdateStorage(contact) {
+  let contactList = JSON.parse(localStorage.getItem("ContactList"))
+  if (contactList != undefined) {
+    contactList.push(contact)
+  } else {
+    contactList = [contact]
+  }
+  alert("Contact Added Sucessfully")
+  localStorage.setItem("contactList",JSON.stringify(contactList))
+}
+
+function createContact() {
+  let contact = new Contact();
+  contact.id = new Date().getTime();
   try {
     contact.name = getInputValueById("#name");
   } catch (error) {
@@ -84,11 +104,11 @@ function save() {
     setTextValue(".zip-error", error);
     throw error;
   }
-
   console.log(contact.toString());
+  return contact
 }
 
 function getInputValueById(property) {
-    let value = document.querySelector(property).value;
-    return value;   
+  let value = document.querySelector(property).value;
+  return value;
 }
